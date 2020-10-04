ff29caf5f04bb41373af4599a44b034ccb1dd182
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-10-04 19:13:13 +0300

    Update admin models

--- name_server/django/name_server/admin.py
+++ name_server/django/name_server/admin.py
@@ -1,5 +1,41 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Storage)
-admin.site.register(File)
+
+@admin.register(Storage)
+class StorageAdmin(admin.ModelAdmin):
+    list_display = (
+        "ip",
+        "status",
+        "num_of_files",
+    )
+
+    readonly_fields = (
+        "ip",
+        "status",
+        "files_",
+    )
+
+    def files_(self, obj):
+        return ', '.join([file.file_path for file in obj.files.all()])
+
+    def num_of_files(self, obj):
+        return obj.files.count()
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = (
+        "file_path",
+        "hash",
+        "num_of_replicas",
+    )
+
+    readonly_fields = (
+        "file_path",
+        "hash",
+        "storage",
+    )
+
+    def num_of_replicas(self, obj):
+        return obj.storage.count()
--- name_server/django/name_server/models.py
+++ name_server/django/name_server/models.py
@@ -11,11 +11,15 @@ class Storage(models.Model):
     ip = models.GenericIPAddressField(
         unique=True
     )
+
     status = models.CharField(
         max_length=2,
         choices=STATUS_CHOICES,
         default='UP',
     )
+
+    def __str__(self):
+        return self.ip
 
 
 class File(models.Model):
@@ -23,9 +27,15 @@ class File(models.Model):
         max_length=1000,
         unique=True,
     )
+
     hash = models.CharField(
         max_length=32,
     )
+
     storage = models.ManyToManyField(
         Storage,
+        related_name="files",
     )
+
+    def __str__(self):
+        return self.file_path
